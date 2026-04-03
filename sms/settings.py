e47f5bbee6b80b422cc1cf58055810b811e2f4a5
unikat7 <tamangunika38@gmail.com>
2026-04-03 11:08:19 +0545

    added allowedd host

--- sms/settings.py
+++ sms/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = [smsii.onrender.com]
+ALLOWED_HOSTS = ['smsii.onrender.com']
 
 
 # Application definition
